efa13181ba47cb553612862f88fe2df3756f3f1d
Jeremias Reinoso <141459619+JeremiasReinoso@users.noreply.github.com>
2026-03-16 14:55:44 -0300

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,10 +1,16 @@
 window.addEventListener("load", function () {
   const loader = document.getElementById("page-loader");
+  const content = document.getElementById("main-content");
   if (!loader) return;
 
   loader.style.opacity = "0";
 
   setTimeout(() => {
     loader.style.display = "none";
+    if (content) {
+      content.style.opacity = "1";
+    }
+    document.body.classList.remove("is-loading");
+    document.body.style.overflow = "auto";
   }, 600);
 });
